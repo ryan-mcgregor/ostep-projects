f73a34e8eeba470f03f72f534203ab87440e345d
ryan-mcgregor <tug81211@temple.edu>
2020-01-28 13:53:52 -0500

    Completed wzip

--- initial-utilities/wzip/wzip.c
+++ initial-utilities/wzip/wzip.c
@@ -5,46 +5,51 @@
 int main(int argc, char *argv[]) {
     if(argc < 2) {
         printf("wzip: file1 [file2 ...]\n");
+        exit(1);
     }
     else {
-        int globalCounter = -2;
+        int globalCounter = -1;
         char globalCurrent;
 
         for (int i = 1; i < argc; ++i) {
-            FILE *fp = fopen(argv[i], "rb");
+            FILE *fp = fopen(argv[i], "r");
             if (fp == NULL) {
                 printf("wzip: cannot open file\n");
+                exit(1);
             }
             else {
                 char current;
                 int counter;
+                if(globalCounter < 0) {
+                    current = fgetc(fp);
+                    counter = 1;
+                }
+                else {
+                    current = globalCurrent;
+                    counter = globalCounter;
+                }
                 while(current != EOF) {
-                    if(globalCounter < 0) {
-                        current = fgetc(fp);
-                        counter = 1;
-                    }
-                    else {
-                        current = globalCurrent;
-                        counter = globalCounter;
-                    }
+
                     char next;
                     while((((next = fgetc(fp)) == current) && next != EOF)) {
                         counter++;
                     }
-                    if (next != EOF || (argc == (i+1))) {
+                    if (next != EOF || (i == (argc - 1))) {
                         fwrite(&counter, 4, 1, stdout);
-                        fwrite(&current, 1, 1, stdout);
+                        fputc(current, stdout);
                         globalCounter = -1;
+                        counter = 1;
                     }
                     else {
                         globalCurrent = current;
                         globalCounter = counter;
                     }
                     current = next;
-
                 }
 
             }
+            fclose(fp);
         }
     }
+    return 0;
 }
